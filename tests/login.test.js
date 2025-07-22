857f6f9d86daca606024d6e2e3157b565797bb0c
Pedro Senna <pedro.senna@onbusdigital.com.br>
2025-07-22 19:51:21 -0300

    adicionando o arquivo inicial com teste de login

---
+++ tests/login.test.js
@@ -0,0 +1,35 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  // Define the number of iterations for the test
+  iterations: 50,
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<90', 'max<150'], // 95% of requests should be below 200ms
+  },
+};
+
+export default function () {
+  const url = 'http://localhost:3000/login';
+  const body = JSON.stringify({
+    username: 'julio.lima',
+    senha: '123456',
+  });
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const response = http.post(url, body, params);
+
+  check(response,{
+    'validar status 200 login com sucesso': (res) => res.status === 200,
+    'verificar se recebe token': (res) =>  'token' in JSON.parse(res.body) 
+  })
+  
+
+  sleep(1);
+}
